36efd8324d375546829bb602f22143ec217dc991
신채원 <cw121@naver.com>
2022-11-12 01:00:53 +0900

    feat: user findByEmail 구현

---
+++ src/main/java/com/example/mentomen/member/service/UserService.java
@@ -0,0 +1,32 @@
+package com.example.mentomen.member.service;
+
+import com.example.mentomen.article.dto.ArticleRequestDto;
+import com.example.mentomen.article.dto.ArticleResponseDto;
+import com.example.mentomen.article.entity.Article;
+import com.example.mentomen.article.repository.ArticleRepository;
+import com.example.mentomen.member.dto.UserDto;
+import com.example.mentomen.member.entity.UserEntity;
+import com.example.mentomen.member.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public UserDto findByEmail(String email) {
+
+        UserEntity user = userRepository.findByEmail(email);
+
+        return new UserDto(user);
+    }
+}
