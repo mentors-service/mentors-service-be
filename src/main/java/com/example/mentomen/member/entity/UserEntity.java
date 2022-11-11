fb523f24f27cca8e103282adaac9ff62e9741529
신채원 <cw121@naver.com>
2022-11-12 00:59:37 +0900

    feat: 프로필을 위한 엔티티 수정

--- src/main/java/com/example/mentomen/member/entity/UserEntity.java
+++ src/main/java/com/example/mentomen/member/entity/UserEntity.java
@@ -5,18 +5,30 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
+@Table(name = "user")
 @NoArgsConstructor //파라미터가 없는 생성자를 생성한다.
 public class UserEntity {
 
     @Id // primary key
+    @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
@@ -26,6 +38,11 @@ public class UserEntity {
 
     private String provider; //google
     private String providerId;
+
+    @NotBlank
+    private String nickname;
+    private String discription;
+
     @CreationTimestamp
     private Timestamp createDate;
 
@@ -38,6 +55,14 @@ public class UserEntity {
         this.provider = provider;
         this.providerId = providerId;
         this.createDate = createDate;
+        this.nickname = username;
+        this.discription = null;
+    }
+
+    public void update(String nickname, String discription) {
+        this.nickname = nickname;
+        if (discription != null)
+            this.discription = discription;
     }
 
 }
